3b7b5cfd67f6a392c0202e132659c0da249d0405
Markus Sandén <markus.g.sanden@gmail.com>
2023-05-17 21:42:09 +0200

    adding the devil's frac

--- main.go
+++ main.go
@@ -16,7 +16,47 @@ var (
 	}
 )
 
+var devilsFrac = frac.Generator{
+	UpScaler: func(pixel int8) frac.Shape {
+		switch pixel {
+		case 1:
+			return frac.Shape{
+				{4, 0, 2},
+				{1, 1, 1},
+				{0, 1, 0},
+			}
+		case 2:
+			return frac.Shape{
+				{0, 2, 1},
+				{2, 2, 0},
+				{0, 2, 3},
+			}
+		case 3:
+			return frac.Shape{
+				{0, 3, 0},
+				{3, 3, 3},
+				{4, 0, 2},
+			}
+		case 4:
+			return frac.Shape{
+				{1, 4, 0},
+				{0, 4, 4},
+				{3, 4, 0},
+			}
+		default:
+			return *frac.NewShape(3, 3)
+		}
+
+	},
+	ShapePixelToColor: func(pixel int8) color.Color {
+		if pixel == 0 {
+			return colorLight
+		}
+		return colorDark
+	},
+}
+
 func main() {
-	img := frac.VFractal.CreateImage(4)
+	img := devilsFrac.CreateImage(4)
 	frac.SaveImage("new-frac.png", img)
 }
