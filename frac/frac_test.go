14bfc5733f52687ad8e75dcaa3f15c0529e60ae6
Markus Sandén <markus@doctrin.se>
2023-05-31 11:26:56 +0200

    remove comments

--- frac/frac_test.go
+++ frac/frac_test.go
@@ -2,10 +2,6 @@ package frac
 
 import "testing"
 
-// 3043531 ns/op
-// 2838700 ns/op
-// 2819896 ns/op
-
 func BenchmarkFrac(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		VFractal.CreateImage(3, 5)
